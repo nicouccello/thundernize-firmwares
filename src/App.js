38160bbf20fbedee59f87d17a4ec4eb9d6e6e2e3
thundernize <nicouccello@gmail.com>
2022-02-05 20:39:23 -0300

    WEB

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          NEXO THUNDER MAKAKO LOCO LOCURUCHO
+         THUNDERNIZE WEB
         </a>
       </header>
     </div>
