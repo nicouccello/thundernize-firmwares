837424c3005c0cd469bc882660084bdd7648bd1a
thundernize <nicouccello@gmail.com>
2022-02-05 20:33:57 -0300

    12

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          NEXO THUNDER DALEEEE GATO MATOS
+          NEXO THUNDER MAKAKO
         </a>
       </header>
     </div>
