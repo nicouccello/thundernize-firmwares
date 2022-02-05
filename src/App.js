05cd4782d6e80ea936555df260fd5de9fd9d4fc3
thundernize <nicouccello@gmail.com>
2022-02-05 19:22:54 -0300

    Actualizacion labels

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://thundernize.com"
+          href="http://thundernize.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          THUNDERNIZE
+          NEXO
         </a>
       </header>
     </div>
